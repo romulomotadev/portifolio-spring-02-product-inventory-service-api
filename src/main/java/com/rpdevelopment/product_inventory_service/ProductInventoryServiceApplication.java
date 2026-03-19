f0132261e0eee589593bac8a2b26c6666f8c544d
Romulo P T Mota <romuloptmota@gmail.com>
2026-03-19 18:02:15 -0300

    chore: initial project setup

---
+++ src/main/java/com/rpdevelopment/product_inventory_service/ProductInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rpdevelopment.product_inventory_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rpdevelopment/product_inventory_service/ProductInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpdevelopment.product_inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
